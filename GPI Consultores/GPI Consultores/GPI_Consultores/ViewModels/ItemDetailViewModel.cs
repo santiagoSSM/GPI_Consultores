04ec3864fb2f0d3b59e6d2829e8fbd05a5a07460
santiagoSSM <santiagosanchezmadrigal1@hotmail.com>
2017-07-17 14:42:09 -0600

    Primer Version

---
+++ GPI Consultores/GPI Consultores/GPI_Consultores/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GPI_Consultores.Models;
+
+namespace GPI_Consultores.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item.Text;
+            Item = item;
+        }
+
+        int quantity = 1;
+        public int Quantity
+        {
+            get { return quantity; }
+            set { SetProperty(ref quantity, value); }
+        }
+    }
+}
---
+++ GPI Consultores/GPI Consultores/GPI_Consultores/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace GPI_Consultores.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
