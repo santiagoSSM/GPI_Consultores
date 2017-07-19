79f4d58f7d074dd662da6d113373e9e99e043a0d
Oscar Jimenez <racso_17@live.com>
2017-07-19 14:53:57 -0600

    LoginInterfaz

---
+++ GPI Consultores/GPI Consultores/GPI_Consultores/Paginas/Login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GPI_Consultores.Paginas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            enterButton.Clicked += EnterButton_Clicked;
+        }
+
+        private async void EnterButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(userLogin.Text))
+            {
+                await DisplayAlert("Error", "Debe ingresar un Usuario", "Aceptar");
+                userLogin.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(userLogin.Text))
+            {
+                await DisplayAlert("Error", "Debe ingresar una Contraseña", "Aceptar");
+                userPass.Focus();
+                return;
+
+            }
+
+
+        }
+    }
+}
