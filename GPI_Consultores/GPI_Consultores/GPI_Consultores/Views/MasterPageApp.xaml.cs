0c7e02ed882797d7a2360a837b1c469b998a7c16
Oscar Jimenez <racso_17@live.com>
2017-08-09 15:56:44 -0600

    MasterPage Cambios

--- GPI_Consultores/GPI_Consultores/GPI_Consultores/Views/MasterPageApp.xaml.cs
+++ GPI_Consultores/GPI_Consultores/GPI_Consultores/Views/MasterPageApp.xaml.cs
@@ -21,8 +21,57 @@ namespace GPI_Consultores.Views
 
         void Init ()
         {
+
+            List<Menu> menu = new List<Menu>()
+            {
+                new Menu { MenuTitle = "Inicio", MenuDetail = "Regresa a la Pagina Principal"},
+                new Menu { MenuTitle = "Opciones", MenuDetail = "Navegar"}
+            };
+
+            ListMenu.ItemsSource = menu;
+
+            Device.OnPlatform(Android: () => {
+
+                ListMenu.Margin = new Thickness(0, 21, 0, 0);
+
+            }, iOS: () => {
+
+                ListMenu.Margin = new Thickness(0, 21, 0, 0);
+
+            });
        
             Detail = new NavigationPage(new ItemsPage());
         }
-	}
+
+        private void ListMenu_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+           var menu = e.SelectedItem as Menu;
+            if (menu != null)
+            {
+                if (menu.MenuTitle.Equals("Inicio"))
+                {
+                    Detail = new NavigationPage( new ItemsPage());
+                }else if (menu.MenuTitle.Equals("Opciones"))
+                {
+                    Detail = new NavigationPage(new ItemsPage());
+                }
+            }
+        }
+    }
+
+    public class Menu
+    {
+
+        public string MenuTitle { 
+            get;
+            set;
+        }
+
+        public string MenuDetail {
+
+            get;
+            set;
+
+        }
+    }
 }
