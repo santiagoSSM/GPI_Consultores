5d3c0423665f19453b982d8c96ae88ab29f43d11
santiagoSSM <santiagosanchezmadrigal1@hotmail.com>
2017-08-31 18:33:16 -0600

    MockServiceAñadido

---
+++ MockService/MockService/TaskList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MockService
+{
+    [DataContract]
+    public static class TaskList
+    {
+        private static List<TaskWS> listTask = new List<TaskWS>();
+
+        [DataMember]
+        public static List<TaskWS> ListTask
+        {
+            get
+            {
+                return listTask;
+            }
+
+            set
+            {
+                listTask = value;
+            }
+        }
+
+        public static void NewTask(TaskWS tarea)
+        {
+            tarea.IdTask = listTask.Count();
+            listTask.Add(tarea);
+        }
+    }
+}
