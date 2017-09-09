16055bcb52f5f96b0c96ecb456882d50ccaf4325
santiagoSSM <santiagosanchezmadrigal1@hotmail.com>
2017-09-08 19:31:23 -0600

    Omitir archivo ConstantsWA.cs

---
+++ GPI_Consultores/GPI_Consultores/GPI_Consultores/WebApi/TaskWA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPI_Consultores.WebApi
+{
+    class TaskWA
+    {
+    }
+}
