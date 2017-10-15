00e78f96b936788371a2a57806db8b36d01d36d5
santiagoSSM <santiagosanchezmadrigal1@hotmail.com>
2017-10-14 22:41:31 -0600

    Eliminacion de proyectos prueba y agregacion de ejemplo de PopUp Xaml

---
+++ PopUp_avanzado/PopUp_avanzado/PopUp_avanzado/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PopUp_avanzado
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            try
+            {
+                var contentPage = new HomePage();
+                MainPage = new NavigationPage(contentPage);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+        }
+    }
+}
---
+++ PopUp_avanzado/PopUp_avanzado/PopUp_avanzado/PropertiesPopup.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+using Rg.Plugins.Popup.Pages;
+
+namespace PopUp_avanzado
+{
+    public partial class PropertiesPopup : PopupPage
+    {
+        public PropertiesPopup()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.PopAsync();
+        }
+    }
+}
