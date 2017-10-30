2c9da0bfd8f59af6dbe83c2b65b243396f77a9ad
santiagoSSM <santiagosanchezmadrigal1@hotmail.com>
2017-10-30 16:01:30 -0600

    cambio en el nombre del bindign tarea, cargar pickers para editar arreglado

--- GPIApp/GPIApp/GPIApp/ViewModels/NewEditTaskViewModel.cs
+++ GPIApp/GPIApp/GPIApp/ViewModels/NewEditTaskViewModel.cs
@@ -15,14 +15,14 @@ namespace GPIApp.ViewModels
     {
         NavigationService navServ;
         public string Title { get; private set; }
-        public TaskUserDraftModel Task { get; set; }
+        public TaskUserDraftModel TaskBind { get; set; }
         public TaskPickersModel TaskPickers { get; set; }
 
         public NewEditTaskViewModel(IVMContainer inter, string title)
         {
             navServ = new NavigationService(inter);
             Title = title;
-            Task = new TaskUserDraftModel();
+            TaskBind = new TaskUserDraftModel();
             TaskPickers = new TaskPickersModel();
         }
 
@@ -39,38 +39,38 @@ namespace GPIApp.ViewModels
 
             //NoTaskAttributes
 
-            Task.IdUser = editTask.IdUser;
-            Task.IsDraft = editTask.IsDraft;
+            TaskBind.IdUser = editTask.IdUser;
+            TaskBind.IsDraft = editTask.IsDraft;
 
             //Task
 
-            Task.IdTask = editTask.IdTask;
-            Task.TextIssue = editTask.TextIssue;
-            Task.TextDescription = editTask.TextDescription;
-            Task.NameRespUser = editTask.NameRespUser;
-            Task.NameCopyUser = editTask.NameCopyUser;
-            Task.TextCategory = editTask.TextCategory;
-            Task.IsAprob = editTask.IsAprob;
-            Task.TextPriority = editTask.TextPriority;
+            TaskBind.IdTask = editTask.IdTask;
+            TaskBind.TextIssue = editTask.TextIssue;
+            TaskBind.TextDescription = editTask.TextDescription;
+            TaskBind.NameRespUser = editTask.NameRespUser;
+            TaskBind.NameCopyUser = editTask.NameCopyUser;
+            TaskBind.TextCategory = editTask.TextCategory;
+            TaskBind.IsAprob = editTask.IsAprob;
+            TaskBind.TextPriority = editTask.TextPriority;
 
             //Recurrence
 
-            Task.TextRecu = editTask.TextRecu;
-            Task.BeforeDays = editTask.BeforeDays;
-            Task.IsCancelRecu = editTask.IsCancelRecu;
+            TaskBind.TextRecu = editTask.TextRecu;
+            TaskBind.BeforeDays = editTask.BeforeDays;
+            TaskBind.IsCancelRecu = editTask.IsCancelRecu;
 
             //Daily,Montly,Annual Vector info
 
-            Task.SelectTimeOfRecu = editTask.SelectTimeOfRecu;
-            Task.TimeOfRecu0 = editTask.TimeOfRecu0;
-            Task.TimeOfRecu1 = editTask.TimeOfRecu1;
-            Task.TimeOfRecu2 = editTask.TimeOfRecu2;
+            TaskBind.SelectTimeOfRecu = editTask.SelectTimeOfRecu;
+            TaskBind.TimeOfRecu0 = editTask.TimeOfRecu0;
+            TaskBind.TimeOfRecu1 = editTask.TimeOfRecu1;
+            TaskBind.TimeOfRecu2 = editTask.TimeOfRecu2;
 
             //Final Date
 
-            Task.TextFinalDate = editTask.TextFinalDate;
-            Task.NumRecu = editTask.NumRecu;
-            Task.ContractExp = editTask.ContractExp;
+            TaskBind.TextFinalDate = editTask.TextFinalDate;
+            TaskBind.NumRecu = editTask.NumRecu;
+            TaskBind.ContractExp = editTask.ContractExp;
         }
 
         public ICommand SaveTaskCommand
@@ -78,7 +78,7 @@ namespace GPIApp.ViewModels
             get { return new RelayCommand<string>(SaveTask); }
         }
 
-        public async void SaveTask(string isDraft)
+        private async void SaveTask(string isDraft)
         {
             //Todo validacion de entradas de informacion
             /*if (string.IsNullOrEmpty(Task.TextIssue))
@@ -115,31 +115,31 @@ namespace GPIApp.ViewModels
             #region Recurrencia
             //Recurrence
 
-            Task.TextRecu = "Ninguna";
-            Task.BeforeDays = 1;
-            Task.IsCancelRecu = false;
+            TaskBind.TextRecu = "Ninguna";
+            TaskBind.BeforeDays = 1;
+            TaskBind.IsCancelRecu = false;
 
             //Daily,Montly,Annual Vector info
 
-            Task.SelectTimeOfRecu = false;
-            Task.TimeOfRecu0 = 0;
-            Task.TimeOfRecu1 = 0;
-            Task.TimeOfRecu2 = 0;
+            TaskBind.SelectTimeOfRecu = false;
+            TaskBind.TimeOfRecu0 = 0;
+            TaskBind.TimeOfRecu1 = 0;
+            TaskBind.TimeOfRecu2 = 0;
 
             //Final Date
 
-            Task.TextFinalDate = "Sin fecha de finalización";
-            Task.NumRecu = 0;
-            Task.ContractExp = new DateTime(2010, 8, 10);
+            TaskBind.TextFinalDate = "Sin fecha de finalización";
+            TaskBind.NumRecu = 0;
+            TaskBind.ContractExp = new DateTime(2010, 8, 10);
             #endregion
 
-            Task.IdUser = UserLogged.Value.IdUser;
-            Task.IsDraft = isDraft == "true";
+            TaskBind.IdUser = UserLogged.Value.IdUser;
+            TaskBind.IsDraft = isDraft == "true";
 
             try
             {
                 await PopupNavigation.PushAsync(new ActivityIndicatorPopUp());//Todo cambiar por guardando
-                if (await TaskWACtrl.Post(Task))
+                if (await TaskWACtrl.Post(TaskBind))
                 {
                     await DialogService.ShowMessage("Mensaje", "Tarea registrada correctamente", "Aceptar");
                     await navServ.Navigate("MainPage");
